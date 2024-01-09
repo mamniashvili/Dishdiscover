d31d366626892f279cd034f2ad2576f29e8ae178
mamniashvili <156017151+mamniashvili@users.noreply.github.com>
2024-01-09 18:22:10 +0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,3 +0,0 @@
-from extensions import app
-from routes import home, login, signup, logout, submit_recipe, profile, recipe, edit_recipe, delete_recipe, \
-    holiday_specials, main_course, desserts, appetizers
