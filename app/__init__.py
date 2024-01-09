32c7436847f33cd57e2f653f48f0502e7115936e
mamniashvili <156017151+mamniashvili@users.noreply.github.com>
2024-01-09 18:20:50 +0400

    Add files via upload

---
+++ app/__init__.py
@@ -0,0 +1,3 @@
+from extensions import app
+from routes import home, login, signup, logout, submit_recipe, profile, recipe, edit_recipe, delete_recipe, \
+    holiday_specials, main_course, desserts, appetizers
